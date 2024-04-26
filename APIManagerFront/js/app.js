95b8a9f8c68dd6b6dc054ee833a8e30a877545a2
FranG <nectryk@gmail.com>
2024-04-26 19:13:01 +0200

    funcionalidad de peticiones

--- APIManagerFront/js/app.js
+++ APIManagerFront/js/app.js
@@ -4,12 +4,76 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.getElementById('method').addEventListener('change', toggleRequestBodyVisibility);
 
+document.getElementById('send').addEventListener('click', sendRequest);
+
 function toggleRequestBodyVisibility() {
     let method = document.getElementById('method').value;
     const bodyContainer = document.getElementById('request-body-container');
-    if(method == "post" || method == "put"){
+    if(method === "POST" || method === "PUT"){
         bodyContainer.style.display = 'block';
     } else {
         bodyContainer.style.display = 'none';
     }
 }
+
+async function sendRequest() {
+    let methodSelected = document.getElementById('method').value;
+    let url = document.getElementById('url').value;
+    const responseDisplay = document.getElementById('response');
+    const body = document.getElementById('request-body').value;
+    console.log(body)
+
+    let conf;
+
+    switch(methodSelected){
+        case "GET":
+            conf = {
+                method: methodSelected
+            }
+            console.log("get")
+            break;
+        case "HEAD":
+            conf = {
+                method: methodSelected
+            }
+            console.log("head")
+            break;
+        case "POST":
+            conf = {
+                method: methodSelected,
+                body: JSON.stringify(body),
+            }
+            console.log("post")
+            break;
+        case "PUT":
+            conf = {
+                method: methodSelected,
+                body: JSON.stringify(body),
+            }
+            console.log("put")
+            break;
+        case "DELETE":
+            conf = {
+                method: methodSelected
+            }
+            console.log("delete")
+            break;
+        default:
+            break;
+    }
+
+    try {
+        console.log(conf);
+        const response = await fetch(url, conf)
+        if (!response.ok) {
+                alert(`HTTP ERROR: ${response.status}`);
+        }
+        
+        const data = await response.json();
+            
+        responseDisplay.value = JSON.stringify(data, null, 2);
+    } catch(error) {
+        responseDisplay.value = error;
+    }
+}
+
