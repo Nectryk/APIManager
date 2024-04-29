34b7a25540f3ca6f5d796c64977ac20fa0ba5645
FranG <nectryk@gmail.com>
2024-04-29 19:22:00 +0200

    adición de autenticación

--- APIManager/wwwroot/js/app.js
+++ APIManager/wwwroot/js/app.js
@@ -2,9 +2,12 @@ const API_SERVER = "http://localhost:5234/";
 
 document.addEventListener('DOMContentLoaded', function() {
     toggleRequestBodyVisibility();
+    authDesign();
 });
 
 document.getElementById('method').addEventListener('change', toggleRequestBodyVisibility);
+
+document.getElementById('auth').addEventListener('change', authDesign);
 
 document.getElementById('send').addEventListener('click', sendRequest);
 
@@ -57,9 +60,14 @@ async function sendRequest() {
         if (!response.ok) {
             throw new Error(`HTTP ERROR: ${response.status}`);
         }
+        console.log(methodSelected)
+        if (methodSelected !== "HEAD") {
+            const data = await response.json();
+            responseDisplay.value = JSON.stringify(data, null, 2);
+        } else {
+            responseDisplay.value = "HEAD request was successful; no body returned.";
+        }
 
-        const data = await response.json();
-        responseDisplay.value = JSON.stringify(data, null, 2);
     } catch (error) {
         console.error('Error:', error);
         responseDisplay.value = `Error: ${error.message}`;
@@ -71,7 +79,15 @@ function addHeader() {
     let newHeader = document.createElement('div');
 
     newHeader.className = 'header-pair row mt-3';
-    newHeader.innerHTML = '<div class="col-sm-3 header-key-container"><input type="text" placeholder="Header Key" class="header-key form-control bg-dark text-white"></div><div class="col-sm-3 header-value-container"><input type="text" placeholder="Header Value" class="header-value form-control bg-dark text-white"></div><div class="col-sm-3"><button type="button" class="btn btn-danger" onclick="removeHeader(this)">🗑</button></div>';
+    newHeader.innerHTML = `<div class="col-sm-3 header-key-container">
+    <input type="text" placeholder="Header Key" class="header-key form-control bg-dark text-white">
+    </div>
+    <div class="col-sm-3 header-value-container">
+    <input type="text" placeholder="Header Value" class="header-value form-control bg-dark text-white">
+    </div>
+    <div class="col-sm-3">
+    <button type="button" class="btn btn-danger" onclick="removeHeader(this)">🗑</button>
+    </div>`;
     headersParent.append(newHeader);
 }
 
@@ -82,6 +98,7 @@ function removeHeader(button) {
 function getHeaders() {
     const headers = {}
     const headersPair = document.querySelectorAll('.header-pair');
+    const authSelected = document.getElementById('auth').value;
     headersPair.forEach(pair => {
         const key = pair.querySelector('.header-key-container').querySelector('.header-key').value.trim();
         const value = pair.querySelector('.header-value-container').querySelector('.header-value').value.trim();
@@ -90,6 +107,40 @@ function getHeaders() {
             headers[key] = value;
         }
     });
+
+    switch (authSelected) {
+        case 'basic':
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value
+            headers['Authorization'] = 'Basic ' + btoa(username + ':' + password);
+            break;
+        case 'bearer':
+        case 'oauth2': 
+            const token = document.getElementById('token').value;
+            headers['Authorization'] = `Bearer ${token}`;
+            break;
+    }
     return headers;
 }
 
+function authDesign() {
+    const auth = document.getElementById('auth').value;
+    const container = document.getElementById('auth-details');
+
+    switch (auth) {
+        case 'basic':
+            container.innerHTML = `
+                <input type="text" id="username" placeholder="Username" class="form-control bg-dark text-white">
+                <input type="password" id="password" placeholder="Password" class="form-control mt-2 bg-dark text-white">
+            `;
+            break;
+        case 'bearer':
+            container.innerHTML = `<input type="text" id="token" placeholder="Token" class="form-control bg-dark text-white">`;
+            break;
+        case 'oauth2':
+            container.innerHTML = `<input type="text" id="token" placeholder="Access Token" class="form-control bg-dark text-white">`;
+            break;
+        default:
+            container.innerHTML = '';
+    }
+}
